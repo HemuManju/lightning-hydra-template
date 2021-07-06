da8113addc1d81446fc4ebf4982fe0febb9d7192
Hemu <hemanthm2277@gmail.com>
2021-07-05 19:06:45 -0500

    Deleted unnecessary files

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/train.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/train.py
@@ -1,22 +1,9 @@
-import numpy as np
+from hydra.experimental import compose, initialize
+from utils import skip_run
 
-import torch
-import pytorch_lightning as pl
+# Initialize the config directory
+initialize(config_path="configs", job_name="learning")
 
-import hydra
-
-from src.models.dqn import DQNModel
-
-
-@hydra.main(config_path='configs', config_name='config')
-def main(hparams):
-    torch.manual_seed(hparams.pytorch_seed)
-    model = DQNModel(hparams)
-    trainer = pl.Trainer(max_epochs=10000, val_check_interval=100)
-    trainer.fit(model)
-
-
-if __name__ == '__main__':
-    torch.manual_seed(0)
-    np.random.seed(0)
-    main()
+with skip_run('skip', 'split_image_folder') as check, check():
+    hparams = compose(config_name="config")
+    raise NotImplementedError
